a58de0ba7ae423cb9e5e255447c73b69b5cce789
nesimiyildiz <nesimi_yildiz@aof.anadolu.edu.tr>
2021-12-25 22:55:55 +0300

    movies between endpoint

--- routes/movie.js
+++ routes/movie.js
@@ -24,12 +24,105 @@ router.post('/', (req, res, next)=> {
   const promise=movie.save();
 
   promise.then((data)=>{
-    res.json({status:1})
+    res.json(data)
   }).catch((err)=>{
     res.json(err)
   })
 
 });
+
+//Tüm Filmleri getir
+
+router.get('/',(req,res,next)=>{
+  const promise=Movie.find({});
+  promise.then((data)=>{
+    res.json(data)
+  }).catch((err)=>{
+    res.json(err)
+  })
+});
+
+//top 10 list
+
+router.get('/top10',(req,res,next)=>{
+  const promise=Movie.find({}).limit(10).sort({imdb_score:-1});
+  promise.then((data)=>{
+    res.json(data)
+  }).catch((err)=>{
+    res.json(err)
+  })
+});
+
+
+//id ye göre filmleri getir
+
+router.get('/:movie_id',(req,res,next)=>{
+  const promise=Movie.findById(req.params.movie_id);
+  promise.then((movie)=>{
+    if(!movie) {
+      next({message:'The Movie Was Not Found',code:999});
+    }
+    res.json(movie)
+  }).catch((err)=> {
+    res.json(err)
+  })
+})
+//güncelleme
+router.put('/:movie_id', (req, res, next) => {
+  const promise = Movie.findByIdAndUpdate(
+      req.params.movie_id,
+      req.body,
+      {
+        new: true
+      }
+  );
+
+  promise.then((movie) => {
+    if (!movie)
+      next({ message: 'The movie was not found.', code: 99 });
+
+    res.json(movie);
+  }).catch((err) => {
+    res.json(err);
+  });
+});
+
+//id ye göre silme
+router.delete('/:movie_id', (req, res, next) => {
+  const promise = Movie.findByIdAndRemove(
+      req.params.movie_id,
+      req.body
+  );
+
+  promise.then((movie) => {
+    if (!movie)
+      next({ message: 'The movie was not found.', code: 99 });
+
+    res.json({status:1});
+  }).catch((err) => {
+    res.json(err);
+  });
+});
+
+//iki yıl arasındaki filmlerin listelenmesi
+//between
+
+router.get('/between/:start_year/:end_year',(req,res,next)=>{
+  const {start_year,end_year}=req.params
+
+  const promise=Movie.find(
+      {
+              //büyükeşit       küçükeşit
+        year:{"$gte":parseInt(start_year),"$lte":parseInt(end_year)}
+      }
+  );
+  promise.then((data)=>{
+    res.json(data)
+  }).catch((err)=>{
+    res.json(err)
+  })
+});
+
 
 
 module.exports = router;
