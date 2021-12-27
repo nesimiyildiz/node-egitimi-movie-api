d9af9bc1b34f43dda48baa0da214e095492d6777
nesimiyildiz <nesimi_yildiz@aof.anadolu.edu.tr>
2021-12-27 22:50:20 +0300

    /GET/:director_id move

--- test/api/movie.test.js
+++ test/api/movie.test.js
@@ -4,6 +4,7 @@ const should=chai.should();
 const server=require('../../app')
 chai.use(chaiHttp);
 let token;
+let movieId;
 describe('/api/movies tests',()=>{
     before((done) => {
       chai.request(server)
@@ -27,4 +28,52 @@ describe('/api/movies tests',()=>{
 
         })
     })
+
+    describe('/POST movie',()=>{
+        it('it should POST a movie',(done)=>{
+            const movie={
+                title:'deneme 2',
+                director_id:'61c77a1aa934e3d820400e80',
+                category:'Komedi',
+                country:'Turkey',
+                year:1950,
+                imdb_score:8
+            }
+            chai.request(server)
+                .post('/api/movies')
+                .send(movie,done())
+                .set('x-access-token',token)
+                .end((err,res)=>{
+                    res.should.have.status(200)
+                    res.body.should.be.a('object')
+                    res.body.should.have.property('title');
+                    res.body.should.have.property('director_id');
+                    res.body.should.have.property('country');
+                    res.body.should.have.property('year');
+                    res.body.should.have.property('imdb_score');
+                    movieId=res.body._id;
+                    done();
+                })
+        })
+    })
+
+    describe('/GET/:director_id move',()=>{
+        it('it should get a movie by th hive id',(done)=>{
+            chai.request(server)
+                .get('/api/movies'+movieId,done())
+                .set('x-access-token',token)
+                .end((err,res)=>{
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('title');
+                    res.body.should.have.property('director_id');
+                    res.body.should.have.property('country');
+                    res.body.should.have.property('year');
+                    res.body.should.have.property('imdb_score');
+                    res.body.should.have.property('_id').eql(movieId);
+                    done();
+                })
+        })
+    })
+
 })
