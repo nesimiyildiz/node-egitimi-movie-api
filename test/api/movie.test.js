ef1778c7726195483cee45b2f90195d704a4900c
nesimiyildiz <nesimi_yildiz@aof.anadolu.edu.tr>
2021-12-27 23:40:59 +0300

    director test

--- test/api/movie.test.js
+++ test/api/movie.test.js
@@ -16,64 +16,111 @@ describe('/api/movies tests',()=>{
       })
     });
 
-    describe('/GET movies',()=>{
-        it('it should GET All the Movies',(done)=>{
-            chai.request(server).get('/api/movies')
-                .set('x-access-token',token)
-                .end((err,res)=>{
-                    res.should.have.status(200)
-                    res.body.should.be.a('array')
+    describe('/GET movies', () => {
+        it('it should GET all the movies', (done) => {
+            chai.request(server)
+                .get('/api/movies')
+                .set('x-access-token', token)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('array');
                     done();
-                })
-
+                });
         })
-    })
+    });
 
-    describe('/POST movie',()=>{
-        it('it should POST a movie',(done)=>{
-            const movie={
-                title:'deneme 2',
-                director_id:'61c77a1aa934e3d820400e80',
-                category:'Komedi',
-                country:'Turkey',
-                year:1950,
-                imdb_score:8
-            }
+    describe('/POST movie', () => {
+        it('it should POST a movie', (done) => {
+            const movie = {
+                title: 'Udemy',
+                director_id: '5a34e1afb8523a78631f8540',
+                category: 'Komedi',
+                country: 'Türkiye',
+                year: 1950,
+                imdb_score: 8
+            };
+
             chai.request(server)
                 .post('/api/movies')
-                .send(movie,done())
-                .set('x-access-token',token)
-                .end((err,res)=>{
-                    res.should.have.status(200)
-                    res.body.should.be.a('object')
-                    res.body.should.have.property('title');
-                    res.body.should.have.property('director_id');
-                    res.body.should.have.property('country');
-                    res.body.should.have.property('year');
-                    res.body.should.have.property('imdb_score');
-                    movieId=res.body._id;
-                    done();
-                })
-        })
-    })
-
-    describe('/GET/:director_id move',()=>{
-        it('it should get a movie by th hive id',(done)=>{
-            chai.request(server)
-                .get('/api/movies'+movieId,done())
-                .set('x-access-token',token)
-                .end((err,res)=>{
+                .send(movie)
+                .set('x-access-token', token)
+                .end((err, res) => {
                     res.should.have.status(200);
                     res.body.should.be.a('object');
                     res.body.should.have.property('title');
                     res.body.should.have.property('director_id');
+                    res.body.should.have.property('category');
+                    res.body.should.have.property('country');
+                    res.body.should.have.property('year');
+                    res.body.should.have.property('imdb_score');
+                    movieId = res.body._id;
+                    done();
+                });
+        });
+    });
+
+    describe('/GET/:movie_id movie', () => {
+        it('it should GET a movie by the given id', (done) => {
+            chai.request(server)
+                .get('/api/movies/' + movieId)
+                .set('x-access-token', token)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('title');
+                    res.body.should.have.property('director_id');
+                    res.body.should.have.property('category');
                     res.body.should.have.property('country');
                     res.body.should.have.property('year');
                     res.body.should.have.property('imdb_score');
                     res.body.should.have.property('_id').eql(movieId);
                     done();
-                })
-        })
-    })
+                });
+        });
+    });
 
+    describe('/PUT/:movie_id movie', () => {
+        it('it should UPDATE a movie given by id', (done) => {
+            const movie = {
+                title: '93creative',
+                director_id: '5a34e1afb8523a78631f8541',
+                category: 'Suç',
+                country: 'Fransa',
+                year: 1970,
+                imdb_score: 9
+            };
+
+            chai.request(server)
+                .put('/api/movies/' + movieId)
+                .send(movie)
+                .set('x-access-token', token)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('title').eql(movie.title);
+                    res.body.should.have.property('director_id').eql(movie.director_id);
+                    res.body.should.have.property('category').eql(movie.category);
+                    res.body.should.have.property('country').eql(movie.country);
+                    res.body.should.have.property('year').eql(movie.year);
+                    res.body.should.have.property('imdb_score').eql(movie.imdb_score);
+
+                    done();
+                });
+        });
+    });
+
+    describe('/DELETE/:movie_id movie', () => {
+        it('it should DELETE a movie given by id', (done) => {
+
+            chai.request(server)
+                .delete('/api/movies/' + movieId)
+                .set('x-access-token', token)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('status').eql(1);
+                    done();
+                });
+        });
+    });
 })
